ab2d9f461083286becc5aaffdd259d0f607a5ee6
Christian Albornoz <92342916+albor77@users.noreply.github.com>
2022-06-29 19:54:10 -0300

    Update integrador.py
    Se agrega función para encontrar el valor máximo de la lista.

--- integrador.py
+++ integrador.py
@@ -12,10 +12,19 @@ def suma(lista):
         for elem in lista:
                 suma1 += elem
         print("La suma total de los valores es: " + str(suma1) + "\n") 
+        
+#funcion Christian
+def maximo(lista):
+    valorMax = 0
+    lista.reverse()
+    valorMax = lista[0]
+    print("El valor máximo cargado en la lista es: " + str(valorMax) + "\n")
 
+        
 
 lista = []
 cargarlista()
 print(lista)
 suma(lista)
-    
+maximo(lista)
+
