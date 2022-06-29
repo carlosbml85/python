e86f47dbb1d805694f5dcedd5739eeb0822d6d8d
Christian Albornoz <92342916+albor77@users.noreply.github.com>
2022-06-29 20:35:39 -0300

    Update integrador.py
    Se modifica función para obtener el valor máximo de una lista.

--- integrador.py
+++ integrador.py
@@ -13,20 +13,12 @@ def suma(lista):
                 suma1 += elem
         print("La suma total de los valores es: " + str(suma1) + "\n") 
         
+        
 #funcion Christian
 def maximo(lista):
-    valorMax = 0
-    lista.reverse()
-    valorMax = lista[0]
+    valorMax = max(lista)
     print("El valor máximo cargado en la lista es: " + str(valorMax) + "\n")
 
-#función Oscar
-def promedio(lista):
-    suma = 0
-    for elem in lista:
-        suma += elem
-    promedioTotal = suma / len(lista)
-    print("El promedio total de los valores cargados en la lista es: " + str(promedioTotal) + "\n")
         
 
 lista = []
@@ -34,5 +26,4 @@ cargarlista()
 print(lista)
 suma(lista)
 maximo(lista)
-promedio(lista)
 
