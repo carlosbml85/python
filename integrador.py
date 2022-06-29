565b67ab6d2e4a8ee732e12d17abaa63a58b2095
carlosbml85 <carlos.bambil.com>
2022-06-29 10:40:01 -0300

    arreglo

--- integrador.py
+++ integrador.py
@@ -2,7 +2,6 @@
 #funcion carlos
 lista = []
 def cargarlista():
-        
         for i in range(5):
                 valor = int(input("Ingrese números enteros: "))
                 lista.append(valor)
