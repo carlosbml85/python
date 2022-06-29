7e4bc7759307d7d94f6bfc56c84b7cf627ecc0a2
carlosbml85 <carlos.bambil.com>
2022-06-29 20:51:39 -0300

    commit carlos

--- integrador.py
+++ integrador.py
@@ -1,11 +1,4 @@
 
-#funcion carlos
-def cargarlista():
-        for i in range(5):
-                valor = int(input("Ingrese números enteros: "))
-                lista.append(valor)
-                i += 1
-
 #funcion fara
 def suma(lista):
         suma1 = 0
@@ -19,11 +12,34 @@ def maximo(lista):
     valorMax = max(lista)
     print("El valor máximo cargado en la lista es: " + str(valorMax) + "\n")
 
-        
+
+
+
+
+#funcion carlos        
+def menu(lista):
+    menuOpcion = 0
+    while menuOpcion != 5:
+        print("La lista cargada es:")
+        print(lista)
+        menuOpcion = (int(input("Ingrese el valor de que operación quiere realizar: \n 1-Sumar \n 2-Calcular Promedio \n 3-Calcular Máximo \n 4-Calcular Minimo \n 5-Salir  \n tipee uno de los valores:" + "\n")))
+        if menuOpcion == 1:
+            suma(lista)
+        elif menuOpcion == 2:
+            promedio(lista)
+        elif menuOpcion == 3:
+            maximo(lista)
+        elif menuOpcion == 4:
+            minimo(lista)
+        else:
+            menuOpcion == 5
+            print("FIN")
+
 
 lista = []
-cargarlista()
-print(lista)
-suma(lista)
-maximo(lista)
+for i in range(5):
+        valor = int(input("Ingrese números enteros: "))
+        lista.append(valor)
+        i += 1
 
+menu(lista)
